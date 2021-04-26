c8ffe55d6589f6a53c6530eef31d6cb78b4b86c8
Stephan Lefort <lefortsge@gmail.com>
2021-04-26 16:20:34 -0700

    chore: Create  Signin, Signup and Checkout components and route them in index.js

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Brews
 
 A full-stack e-commerce app from scratch using React, GraphQL, Stripe, and the Headless CMS Strapi.
+This app will demonstrate on how to Build an ecommerce app using the above technologies.
 
 ## Get Started
 
@@ -16,3 +17,8 @@ or:
 Run either `npm start` or `yarn start` depending on the method of installing the dependencies above.
 
 By now, the `http://localhost:3000` should be up and running.
+
+## Steps
+
+Create 3 components for Signin, Signup and Checkout in the components folder.
+Route the 3 components including the App component in the `index.js` file by using `BrowserRouter as Router, Switch and Route` which is imported from `react-router-dom`.
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import App from './components/App';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Checkout from './components/Checkout';
+
+import './index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+            <Router>
+        <Switch>
+            <Route component={App} exact path='/' />
+            <Route component={Signin} path='/signin' />
+            <Route component={Signup} path='/signup' />
+            <Route component={Checkout} path='/checkout' />
+        </Switch>
+    </Router>
+    )
+
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
