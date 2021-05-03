9710b9e36d51cf3efbe4a8aa4f33845ae37d328d
Stephan Lefort <lefortsge@gmail.com>
2021-05-03 14:39:14 -0700

    chore: Retrieve Brands data from Strapi and render them on the homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,91 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Box, Heading, Text, Card, Image } from 'gestalt';
+import Strapi from 'strapi-sdk-javascript/build/main';
 
-import './App.css';
+const apiUrl = process.env.API_URL || 'http://localhost:1337';
+const strapi = new Strapi(apiUrl);
+// import './App.css';
 
 class App extends Component {
+  state = {
+    brands: []
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await strapi.request('POST', '/graphql', {
+        data: {
+          query: `query {
+            brands {
+            _id
+              name
+              description
+              image {
+                url
+              }
+            }
+          }`
+        }
+      });
+      this.setState({ brands: data.brands })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
+    const { brands } = this.state;
+
     return (
-      <div>
-       App
-      </div>
+      <Container>
+        {/* Brands Section */}
+        <Box
+          display='flex'
+          justifyContent='center'
+          marginBottom={2}
+        >
+          {/* Brand Header */}
+          <Heading color='midnight' size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+        {/* Brands */}
+        <Box display="flex" justifyContent="around">
+          {brands.map(brand => (
+            <Box width={200} height={200} key={brand._id}>
+              <Card
+                image={
+                  <Box height={200} width={200}>
+                    <Image
+                      alt="Brand"
+                      naturalHeight={1}
+                      naturalWidth={1}
+                      src={`${apiUrl}${brand.image[0].url}`}
+                    />
+                  </Box>
+                }
+              >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">{brand.name}</Text>
+                  <Box height={80}>
+                    <Text>{brand.description}</Text>
+                  </Box>
+                  <Text bold size="xl">
+                    <Link to={`/${brand._id}`}>See Brews</Link>
+                  </Text>
+
+                </Box>
+              </Card>
+            </Box>
+          ))}
+        </Box>
+      </Container>
     );
   }
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,25 +4,25 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <Box 
-            display="flex" 
-            alignItems="center" 
-            justifyContent="around" 
-            height={70} 
-            color="midnight" 
-            padding={1} 
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="around"
+            height={70}
+            color="midnight"
+            padding={1}
             shape='roundedBottom'
         >
             {/* Sign In Link */}
-            <NavLink  id='text' activeClassName='active' to='/signin'>
-                <Text size='x1' color='white'>Sign In</Text>
+            <NavLink id='text' activeClassName='active' to='/signin'>
+                <Text size='xl' color='white'>Sign In</Text>
             </NavLink>
 
             {/* Title and Logo */}
             <NavLink id='text' activeClassName='active' exact to='/'>
                 <Box display="flex" alignItems="center">
                     <Box margin={2} width={50} height={50}>
-                        <Image 
+                        <Image
                             alt="BrewHaha Logo"
                             naturalHeight={1}
                             naturalWidth={1}
@@ -37,7 +37,7 @@ const Navbar = () => {
 
             {/* Sign Up Link */}
             <NavLink id='text' activeClassName='active' to='/signup'>
-                <Text size='x1' color='white'>Sign Up</Text>
+                <Text size='xl' color='white'>Sign Up</Text>
             </NavLink>
 
         </Box>
