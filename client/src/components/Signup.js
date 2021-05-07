0419279959b408692cbbe48fdd30d1e28a194e44
Stephan Lefort <lefortsge@gmail.com>
2021-05-07 11:01:16 -0700

    feat: Create Sign up Form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,91 @@
 import React from 'react';
+import { Container, Box, Heading, Text, TextField, Button } from 'gestalt';
 
 class Signup extends React.Component {
-    render () {
-        return (<div>Signup</div>)
+
+    state = {
+        username: "",
+        email: "",
+        password: ""
+    }
+
+    handleChange = ({ event, value }) => {
+        event.persist();
+        this.setState({
+            [event.target.name]: value
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <Box
+                    dangerouslySetInlineStyle={{
+                        __style: {
+                            backgroundColor: "#ebe2da"
+                        }
+                    }}
+                    margin={4}
+                    padding={4}
+                    shape="rounded"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    {/* Sign up Form */}
+                    <form style={{
+                        display: "inlineBlock",
+                        textAlign: "center",
+                        maxwidth: 450
+                    }}>
+                        {/* Sign Up Heading */}
+                        <Box
+                            marginBottom={2}
+                            display="flex"
+                            direction="column"
+                            alignItems="center"
+                        >
+                            <Heading color="midnight">Let's Get Started'</Heading>
+                            <Text italic color="orchid">Sign up to order some brews</Text>
+                        </Box>
+                        {/* Username input */}
+                        <TextField
+                            id="username"
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                            onChange={this.handleChange}
+                        />
+                        {/* Email Input */}
+                        <TextField
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            onChange={this.handleChange}
+                        />
+                        {/* Password Input */}
+                        <TextField
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={this.handleChange}
+                        />
+                        <Button
+                            inline
+                            color="blue"
+                            text="Submit"
+                            type="submit"
+                            onClick={() => this.handleSubmit}
+                        />
+
+                    </form>
+
+                </Box>
+
+            </Container>
+        );
     }
 }
 
