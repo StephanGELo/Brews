5cb70cd40f6ff7eac2136ca7547ed747c9d4a544
Stephan Lefort <lefortsge@gmail.com>
2021-05-07 11:23:23 -0700

    chore: Add form validation to Sign Up form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -7,13 +7,24 @@ class Signup extends React.Component {
         username: "",
         email: "",
         password: ""
-    }
+    };
 
     handleChange = ({ event, value }) => {
         event.persist();
         this.setState({
             [event.target.name]: value
         })
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if (!this.isFormEmpty(this.state)) {
+            console.log("submitted")
+        }
+    };
+
+    isFormEmpty = ({ username, email, password }) => {
+        return !username || !email || !password;
     }
 
     render() {
@@ -37,7 +48,9 @@ class Signup extends React.Component {
                         display: "inlineBlock",
                         textAlign: "center",
                         maxwidth: 450
-                    }}>
+                    }}
+                        onSubmit={this.handleSubmit}
+                    >
                         {/* Sign Up Heading */}
                         <Box
                             marginBottom={2}
@@ -77,13 +90,10 @@ class Signup extends React.Component {
                             color="blue"
                             text="Submit"
                             type="submit"
-                            onClick={() => this.handleSubmit}
                         />
 
                     </form>
-
                 </Box>
-
             </Container>
         );
     }
