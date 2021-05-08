b213b765197ee2c2dbfad132f33c10ca190a5e74
Stephan Lefort <lefortsge@gmail.com>
2021-05-08 01:28:03 -0700

    chore: Change Navbar upon Sign Up

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,59 @@
 import React from 'react';
-import { Box, Text, Heading, Image } from 'gestalt';
+import { Box, Text, Heading, Image, Button } from 'gestalt';
 import { NavLink } from 'react-router-dom';
 
+import { getToken } from '../utils';
+
 const Navbar = () => {
+    return getToken() !== null ? <AuthNav /> : <UnAuthNav />;
+};
+
+const AuthNav = () => {
+    return (
+        <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="around"
+            height={70}
+            color="midnight"
+            padding={1}
+            shape='roundedBottom'
+        >
+            {/* Checkout Link */}
+            <NavLink id='text' activeClassName='active' to='/checkout'>
+                <Text size='xl' color='white'>Checkout</Text>
+            </NavLink>
+
+            {/* Title and Logo */}
+            <NavLink id='text' activeClassName='active' exact to='/'>
+                <Box display="flex" alignItems="center">
+                    <Box margin={2} width={50} height={50}>
+                        <Image
+                            alt="BrewHaha Logo"
+                            naturalHeight={1}
+                            naturalWidth={1}
+                            src="icons/logo.svg"
+                        />
+                    </Box>
+                    <Heading size="xs" color="orange">
+                        BrewHaha
+                    </Heading>
+                </Box>
+            </NavLink>
+
+            {/* Signout Button*/}
+            <Button
+                color="transparent"
+                text="Sign Out"
+                inline
+                size="md"
+            />
+        </Box>
+    )
+
+};
+
+const UnAuthNav = () => {
     return (
         <Box
             display="flex"
