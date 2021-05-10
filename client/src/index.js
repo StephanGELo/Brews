41a702f0047b16216caa26c63395a5d8bc34acf6
Stephan Lefort <lefortsge@gmail.com>
2021-05-10 12:03:10 -0700

    chore: Make Checkout Component a PrivateRoute

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import App from './components/App';
 import Navbar from './components/Navbar';
@@ -8,10 +8,21 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import Brews from './components/Brews';
+import { getToken } from './utils';
 
 import 'gestalt/dist/gestalt.css';
 
 import registerServiceWorker from './registerServiceWorker';
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        getToken() !== null ?
+        <Component {...props} /> : <Redirect to={{
+            pathname: './signin',
+            state: {from: props.location}
+        }}/>
+    )}/>
+);
 
 const Root = () => {
     return (
@@ -22,7 +33,7 @@ const Root = () => {
                     <Route component={App} exact path='/' />
                     <Route component={Signin} path='/signin' />
                     <Route component={Signup} path='/signup' />
-                    <Route component={Checkout} path='/checkout' />
+                    <PrivateRoute component={Checkout} path='/checkout' />
                     <Route component={Brews} path='/:brandId' />
                 </Switch>
             </React.Fragment>
