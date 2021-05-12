d77f15b1d1841b88be67d975482ed881a8f167d6
Stephan Lefort <lefortsge@gmail.com>
2021-05-11 18:00:57 -0700

    chore: Create Orders Content Type and Add Create Function in Orders Controllers

---
+++ server/api/orders/controllers/orders.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { default: createStrapi } = require("strapi");
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripe = require('stripe')(`${stripeSecretKey}`);
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    async create(ctx) {
+        const { address, amount, brews, postalCode, token, city } = ctx.request.body;
+
+        // Send charges to Stripe
+        const charge = await stripe.charges.create({
+            amount: amount * 100,
+            currency: 'usd',
+            description: `Order ${new Date(Date.now())} - User ${ctx.state.user._id}`,
+            source: token
+        });
+
+        // Create order in database
+        const order = await strapi.services.orders.add({
+            user: ctx.state.user._id,
+            address,
+            amount,
+            brews,
+            postalCode,
+            city
+        });
+
+        return order;
+    }
+};
